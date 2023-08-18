e930d9ac01edc151d9ca29825d234fbee29c7073
HumamHusamEldeen <humamhusameldeen.github@gmail.com>
2023-08-18 16:12:47 +0300

    Revert sorting chat messages

--- internal/pkg/service/get_chat.go
+++ internal/pkg/service/get_chat.go
@@ -7,8 +7,5 @@ func (s *mvmService) GetChat(userId1, userId2 string) (*mvmPb.Chat, error) {
 	if err != nil {
 		return nil, err
 	}
-	for i, j := 0, len(chat.Messages)-1; i < j; i, j = i+1, j-1 {
-		chat.Messages[i], chat.Messages[j] = chat.Messages[j], chat.Messages[i]
-	}
 	return chat, err
 }
